bb2156ba63fd0861927afcbb97cda863cee05298
Kevin Gasperich <keg56@pitt.edu>
2017-07-21 17:52:15 -0400

    minor changes to potential test

--- pimc/bclark/option2/PotentialTest.py
+++ pimc/bclark/option2/PotentialTest.py
@@ -8,10 +8,13 @@ numParticles=2
 Path=PathClass(numpy.zeros((numTimeSlices,numParticles,3),float),tau,lam)
 Path.SetPotential(HarmonicOscillator)
 Path.SetCouplingConstant(0.0)
-#numSteps=50000
-numSteps=100
-print(PIMC(numSteps,Path,[SingleSliceMove],"SingleSlice"))
+numSteps=50000
+#numSteps=500000
+#numSteps=5000000
+#print(PIMC(numSteps,Path,[SingleSliceMove,DisplaceMove],"SingleSlice"))
+print(PIMC(numSteps,Path,[SingleSliceMove,DisplaceMove,WholeSliceMove],"SingleSlice"))
 import math
 def coth(x):
     return math.cosh(x)/math.sinh(x)
-print("exact",3/2*1.0*coth(numTimeSlices*tau/2))
+#print("exact",3/2*1.0*coth(numTimeSlices*tau/2))
+print("exact",numParticles*3/2*1.0*coth(numTimeSlices*tau/2))
